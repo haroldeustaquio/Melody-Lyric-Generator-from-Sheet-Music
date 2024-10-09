8a76ba4f01096211c9bda175152d20d35e35b311
harold1735 <harold.eustaquio.a@uni.pe>
2024-10-09 00:18:19 -0600

    update obtain_lyrics: in the next steps, add more artist

--- Lyrics-Generation/Genius API/obtain_lyrics.py
+++ Lyrics-Generation/Genius API/obtain_lyrics.py
@@ -3,6 +3,6 @@ import functions
 
 importlib.reload(functions)
 
-functions.upload_song_data("Jose Jose")
-functions.upload_song_data("Juan Gabriel")
-functions.upload_song_data("Sin Bandera")
+functions.upload_song_data("Rio Roma")
+# functions.upload_song_data("Grupo 5")
+# functions.upload_song_data("")
