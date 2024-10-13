1fd169d36c081ff7a1d58194ea27c634f91ebc75
harold1735 <harold.eustaquio.a@uni.pe>
2024-10-13 02:30:59 -0600

    save music of Gian Marco

--- Lyrics-Generation/Genius API/obtain_lyrics.py
+++ Lyrics-Generation/Genius API/obtain_lyrics.py
@@ -3,6 +3,6 @@ import functions
 
 importlib.reload(functions)
 
-functions.upload_song_data("Rio Roma")
+functions.upload_song_data("Gian Marco")
 # functions.upload_song_data("Grupo 5")
 # functions.upload_song_data("")
