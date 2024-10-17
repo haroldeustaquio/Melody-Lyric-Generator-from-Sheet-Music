93dade760c89626f8de9fb4d112ac440c9ddf9cd
harold1735 <harold.eustaquio.a@uni.pe>
2024-10-17 09:17:35 -0600

    Add upload_song_data function to retrieve and process song lyrics from Genius API.

--- Lyrics-Generation/Genius API/obtain_lyrics.py
+++
@@ -1,8 +0,0 @@
-import importlib
-import functions
-
-importlib.reload(functions)
-
-functions.upload_song_data("Gian Marco")
-# functions.upload_song_data("Grupo 5")
-# functions.upload_song_data("")
